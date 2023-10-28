2b69b1bb8a7e03ab52d45fd472b864cfd8d09e7a
Tanay Pai <tanayvpai@gmail.com>
2023-10-27 18:12:25 -0700

    Deploy 10/27 6:12PM

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7245cf4b18e4d4b352ebb7bc6492f169.js"
+  "./precache-manifest.7b95f1ea624787aaf584df775027f46b.js"
 );
 
 self.addEventListener('message', (event) => {
